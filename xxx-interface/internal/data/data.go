bb78df3e3ef59bd120bed34ea7c35221fc0c51dc
penghuihui <phh2008@vip.qq.com>
2025-06-14 23:33:37 +0800

    upload demo

--- xxx-interface/internal/data/data.go
+++ xxx-interface/internal/data/data.go
@@ -1,117 +1,117 @@
 package data
 
 import (
-    "context"
-    "example.com/xxx/common-lib/orm"
-    "example.com/xxx/common-lib/oss"
-    "example.com/xxx/common-lib/oss/storage"
-    "example.com/xxx/interface/internal/conf"
-    userv1 "example.com/xxx/user-service/api/user/v1"
-    "github.com/go-kratos/kratos/contrib/registry/consul/v2"
-    "github.com/go-kratos/kratos/v2/middleware/recovery"
-    "github.com/go-kratos/kratos/v2/registry"
-    "github.com/go-kratos/kratos/v2/transport/grpc"
-    "github.com/hashicorp/consul/api"
-    "gorm.io/gorm"
-    "log/slog"
+	"context"
+	"example.com/xxx/common-lib/orm"
+	"example.com/xxx/common-lib/oss"
+	"example.com/xxx/common-lib/oss/storage"
+	"example.com/xxx/interface/internal/conf"
+	userv1 "example.com/xxx/user-service/api/user/v1"
+	"github.com/go-kratos/kratos/contrib/registry/consul/v2"
+	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/registry"
+	"github.com/go-kratos/kratos/v2/transport/grpc"
+	"github.com/hashicorp/consul/api"
+	"gorm.io/gorm"
+	"log/slog"
 
-    "github.com/google/wire"
+	"github.com/google/wire"
 )
 
 // ProviderSet is data providers.
 var ProviderSet = wire.NewSet(
-    //NewDb,
-    NewData,
-    NewDiscovery,
-    NewRegistrar,
-    NewUserServiceClient,
-    NewRoleServiceClient,
-    NewDemoRepo,
-    NewAttachmentRepo,
-    NewStorage,
+	//NewDb,
+	NewData,
+	NewDiscovery,
+	NewRegistrar,
+	NewUserServiceClient,
+	NewRoleServiceClient,
+	NewDemoRepo,
+	NewAttachmentRepo,
+	NewStorage,
 )
 
 // Data .
 type Data struct {
-    //DB *gorm.DB
-    uc userv1.UserClient
-    rc userv1.RoleClient
-    sc storage.Storage
+	//DB *gorm.DB
+	uc userv1.UserClient
+	rc userv1.RoleClient
+	sc storage.Storage
 }
 
 func NewDb(c *conf.Bootstrap) *gorm.DB {
-    return orm.NewDB(c.Data.Database.Source)
+	return orm.NewDB(c.Data.Database.Source)
 }
 
 // NewData .
 func NewData(
-    c *conf.Bootstrap,
-    uc userv1.UserClient,
-    rc userv1.RoleClient,
+	c *conf.Bootstrap,
+	uc userv1.UserClient,
+	rc userv1.RoleClient,
 ) (*Data, func(), error) {
-    cleanup := func() {
-        slog.Info("closing the data resources")
-    }
-    return &Data{uc: uc, rc: rc}, cleanup, nil
+	cleanup := func() {
+		slog.Info("closing the data resources")
+	}
+	return &Data{uc: uc, rc: rc}, cleanup, nil
 }
 
 func NewStorage() storage.Storage {
-    // 本地文件存储
-    return oss.NewStorage(&storage.Config{BaseFolder: "files"})
+	// 本地文件存储
+	return oss.NewStorage(&storage.Config{OssType: storage.TypeFileSystem, BaseFolder: "files"})
 }
 
 func NewDiscovery(conf *conf.Bootstrap) registry.Discovery {
-    c := api.DefaultConfig()
-    c.Address = conf.Registry.Consul.Address
-    c.Scheme = conf.Registry.Consul.Scheme
-    cli, err := api.NewClient(c)
-    if err != nil {
-        panic(err)
-    }
-    r := consul.New(cli, consul.WithHealthCheck(false))
-    return r
+	c := api.DefaultConfig()
+	c.Address = conf.Registry.Consul.Address
+	c.Scheme = conf.Registry.Consul.Scheme
+	cli, err := api.NewClient(c)
+	if err != nil {
+		panic(err)
+	}
+	r := consul.New(cli, consul.WithHealthCheck(false))
+	return r
 }
 
 func NewRegistrar(conf *conf.Bootstrap) registry.Registrar {
-    c := api.DefaultConfig()
-    c.Address = conf.Registry.Consul.Address
-    c.Scheme = conf.Registry.Consul.Scheme
-    cli, err := api.NewClient(c)
-    if err != nil {
-        panic(err)
-    }
-    r := consul.New(cli, consul.WithHealthCheck(false))
-    return r
+	c := api.DefaultConfig()
+	c.Address = conf.Registry.Consul.Address
+	c.Scheme = conf.Registry.Consul.Scheme
+	cli, err := api.NewClient(c)
+	if err != nil {
+		panic(err)
+	}
+	r := consul.New(cli, consul.WithHealthCheck(false))
+	return r
 }
 
 func NewUserServiceClient(r registry.Discovery) userv1.UserClient {
-    conn, err := grpc.DialInsecure(
-        context.Background(),
-        grpc.WithEndpoint("discovery:///user.service"),
-        grpc.WithDiscovery(r),
-        grpc.WithMiddleware(
-            recovery.Recovery(),
-        ),
-    )
-    if err != nil {
-        panic(err)
-    }
-    c := userv1.NewUserClient(conn)
-    return c
+	conn, err := grpc.DialInsecure(
+		context.Background(),
+		grpc.WithEndpoint("discovery:///user.service"),
+		grpc.WithDiscovery(r),
+		grpc.WithMiddleware(
+			recovery.Recovery(),
+		),
+	)
+	if err != nil {
+		panic(err)
+	}
+	c := userv1.NewUserClient(conn)
+	return c
 }
 
 func NewRoleServiceClient(r registry.Discovery) userv1.RoleClient {
-    conn, err := grpc.DialInsecure(
-        context.Background(),
-        grpc.WithEndpoint("discovery:///user.service"),
-        grpc.WithDiscovery(r),
-        grpc.WithMiddleware(
-            recovery.Recovery(),
-        ),
-    )
-    if err != nil {
-        panic(err)
-    }
-    c := userv1.NewRoleClient(conn)
-    return c
+	conn, err := grpc.DialInsecure(
+		context.Background(),
+		grpc.WithEndpoint("discovery:///user.service"),
+		grpc.WithDiscovery(r),
+		grpc.WithMiddleware(
+			recovery.Recovery(),
+		),
+	)
+	if err != nil {
+		panic(err)
+	}
+	c := userv1.NewRoleClient(conn)
+	return c
 }
--- xxx-interface/internal/service/attachment.go
+++ xxx-interface/internal/service/attachment.go
@@ -1,28 +1,45 @@
 package service
 
 import (
-    "context"
-    "example.com/xxx/interface/internal/biz"
-    "mime/multipart"
+	"context"
+	"example.com/xxx/common-lib/oss/storage"
+	"example.com/xxx/interface/internal/biz"
+	"github.com/jinzhu/copier"
+	"mime/multipart"
+	"time"
 )
 
 type AttachmentService struct {
-    attachmentUseCase *biz.AttachmentUseCase
+	attachmentUseCase *biz.AttachmentUseCase
+	store             storage.Storage
 }
 
-func NewAttachmentService(attachmentUseCase *biz.AttachmentUseCase) *AttachmentService {
-    return &AttachmentService{attachmentUseCase: attachmentUseCase}
+func NewAttachmentService(attachmentUseCase *biz.AttachmentUseCase, store storage.Storage) *AttachmentService {
+	return &AttachmentService{attachmentUseCase: attachmentUseCase, store: store}
 }
 
 type UploadReq struct {
-    File       multipart.File
-    FileHeader *multipart.FileHeader
+	File       multipart.File
+	FileHeader *multipart.FileHeader
 }
 
 type UploadResp struct {
+	Path         string    `json:"path"`         // 文件路径包含文件名(不包括根目录    )
+	Name         string    `json:"name"`         // 文件名
+	LastModified time.Time `json:"lastModified"` // 最后修改时间
+	Size         int64     `json:"size"`         // 文件大小
 }
 
 func (a *AttachmentService) Upload(ctx context.Context, req *UploadReq) (*UploadResp, error) {
-    // TODO 保存文件
-    return nil, nil
+	// 保存文件
+	fileName := req.FileHeader.Filename
+	file := req.File
+	defer file.Close()
+	obj, err := a.store.Put("test/"+fileName, file)
+	if err != nil {
+		return nil, err
+	}
+	var resp UploadResp
+	_ = copier.Copy(&resp, obj)
+	return &resp, nil
 }
